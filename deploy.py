f2d3d125dff873ddd46964ea25f18fd487d9e2b8
Robert Klink <roberthklink@gmail.com>
2026-01-04 02:20:11 +0100

    ansible: registry setup on ctf, add deploy.py, minor fixes/tweaks

---
+++ deploy.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import secrets
+from argparse import ArgumentParser, Namespace
+from termcolor import colored
+
+
+def abort():
+    """Aborts deployment."""
+    print(colored("Aborting deployment", "red"))
+    sys.exit(1)
+
+
+def create_env(args: Namespace):
+    """Creates .env.yml for ansible and .env for ctfd-setup."""
+    print(colored("Creating .env files...", "blue"))
+
+    def input_with_default(prompt: str, default: str | None) -> str:
+        return (
+            default
+            if default is not None
+            else input(colored(f"\t{prompt}: ", "cyan")).strip()
+        )
+
+    ctf_domain = input_with_default(
+        "Enter CTF domain (e.g. ctf.example.com)", args.ctf_domain
+    )
+    challs_domain = input_with_default(
+        "Enter challenges domain (e.g. challs.example.com)", args.challs_domain
+    )
+    cert_email = input_with_default(
+        "Enter cert email (e.g. cert@example.com)", args.cert_email
+    )
+
+    with open("ansible/.env.yml", "w") as f:
+        f.writelines(
+            [
+                f"CTF_DOMAIN: {ctf_domain}\n",
+                f"CHALLS_DOMAIN: {challs_domain}\n",
+                f"CERT_EMAIL: {cert_email}\n",
+                "REGISTRY_USER: admin\n",
+                f"REGISTRY_PASS: {secrets.token_urlsafe(32)}\n",
+                f"SECRET_KEY: {secrets.token_urlsafe(32)}\n",
+            ]
+        )
+
+    with open("ansible/ctfd/ctfd-setup/.env", "w") as f:
+        f.write(f"export ADMIN_PASSWORD='{secrets.token_urlsafe(32)}'\n")
+
+    print(colored(".env files created!", "green"))
+
+
+def setup_remote_nodes():
+    """Sets up remote nodes using Ansible."""
+
+    def run_playbook(name: str):
+        print(colored(f"\tRunning Ansible playbook: {name}", "cyan"))
+        subprocess.run(
+            [
+                "ansible-playbook",
+                "-i",
+                "ansible/inventory.ini",
+                f"ansible/{name}.yml",
+            ],
+            check=True,
+        )
+
+    try:
+        print(colored("Setting up remote nodes...", "blue"))
+        run_playbook("setup-ctf")
+        run_playbook("setup-challs")
+        print(colored("Remote nodes setup successfully", "green"))
+    except subprocess.CalledProcessError as e:
+        print(colored(f"Ansible playbook failed: {e}", "red"))
+        abort()
+
+
+def get_parser() -> ArgumentParser:
+    """Parses command line arguments."""
+    parser = ArgumentParser(
+        description="Deploy CTF environment.",
+    )
+
+    parser.add_argument(
+        "--force-env",
+        action="store_true",
+        help="Forcibly create .env files",
+    )
+
+    parser.add_argument(
+        "--provision",
+        action="store_true",
+        help="Provision remote nodes (currently not implemented)",
+    )
+
+    parser.add_argument(
+        "--setup",
+        action="store_true",
+        help="Setup remote nodes using Ansible",
+    )
+    parser.add_argument(
+        "--cert-email",
+        type=str,
+        help="Email for SSL certificate registration",
+    )
+    parser.add_argument(
+        "--ctf-domain",
+        type=str,
+        help="Domain for the CTF platform",
+    )
+    parser.add_argument(
+        "--challs-domain",
+        type=str,
+        help="Domain for the challenges",
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if not any(v for _, v in args._get_kwargs()):
+        # No arguments provided, print help and exit
+        parser.print_help()
+        sys.exit(0)
+
+    if (
+        args.force_env
+        or (not os.path.exists("ansible/.env.yml"))
+        or (not os.path.exists("ansible/ctfd/ctfd-setup/.env"))
+    ):
+        create_env(args)
+
+    if args.provision:
+        print(colored("Currently not implemented", "red"))
+        abort()
+
+    if args.setup:
+        setup_remote_nodes()
